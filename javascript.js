843b51a4cdbbac4d5c05e1ca1f7a5f969eb59c4e
Daniel <Dan.Berwick44+odinproject@gmail.com>
2023-10-30 17:18:53 -0700

    Creates variable grid with hover functionality

--- javascript.js
+++ javascript.js
@@ -1,18 +1,29 @@
 
 
-function buildEtchasketch(size){
+function buildEtchasketch(){
+    function chooseSize(){
+        var size = parseInt(prompt("Choose a size"))
+        console.log("size recognized as " + typeof size)
+        if ((size > 100) || (size < 0) || (typeof size != "number")){
+            var size = chooseSize()
+        }
+        return size
+    }
+    let size = chooseSize()
     console.log("Button Press Detected")
+    console.log("Button passed size " + size + " grid")
     const container = document.getElementById("container");
     const columnArray = []
-    for (let i = 0; i < Math.sqrt(size); i++){ //Creates the grid columns
-        let activeColumn = document.createElement("span")
-        activeColumn.appendChild(container).class = "col" + i;
+    for (let i = 0; i < size; i++){ //Creates the grid columns
+        let activeColumn = document.createElement("div")
+        container.appendChild(activeColumn)
+        activeColumn.classList.add("column");
         columnArray.push(activeColumn)
         console.log("Column created!")
-        for (let n = 0; n < Math.sqrt(size); n++){
+        for (let n = 0; n < size; n++){
             let activeDiv = document.createElement("div");
             activeDiv.classList.add("row");
-            activeDiv.appendChild(container);
+            activeColumn.appendChild(activeDiv);
             console.log("Row segment created!");
         }
     
